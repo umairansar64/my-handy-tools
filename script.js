87ff8c8fb33af378aade180d65df00ed4c87b58a
Mohammad Umair Ansar <umairanser@hotmail.com>
2023-09-12 20:58:38 +0100

    Add new tool page How Many Days, Update Nav in all pages, Update Styles and Javascript

--- script.js
+++ script.js
@@ -64,3 +64,37 @@ function calculateProportion() {
     // Display the result
     document.getElementById("result").value = aValue;
 }
+
+// How many Days Page Controls
+
+function calculateDaysDifference() {
+    const startDateInput = document.getElementById("startDate").value;
+    const endDateInput = document.getElementById("endDate").value;
+
+    const [startDay, startMonth, startYear] = startDateInput.split("/").map(Number);
+    const [endDay, endMonth, endYear] = endDateInput.split("/").map(Number);
+
+    const startDate = new Date(startYear, startMonth - 1, startDay);
+    const endDate = new Date(endYear, endMonth - 1, endDay);
+
+    // Calculate total days difference
+    const daysDifference = Math.round((endDate - startDate) / (1000 * 60 * 60 * 24));
+
+    // Calculate difference in years, months, and days
+    let yearsDifference = endYear - startYear;
+    let monthsDifference = endMonth - startMonth;
+    let dateDifference = endDay - startDay;
+
+    if (dateDifference < 0) {
+        monthsDifference--;
+        dateDifference += new Date(startYear, startMonth, 0).getDate();
+    }
+    if (monthsDifference < 0) {
+        yearsDifference--;
+        monthsDifference += 12;
+    }
+
+    document.getElementById("ymdResult").value = `${yearsDifference} Years, ${monthsDifference} Months, ${dateDifference} Days`;
+    document.getElementById("mdResult").value = `${(yearsDifference * 12) + monthsDifference} Months, ${dateDifference} Days`;
+    document.getElementById("dResult").value = `${daysDifference} Days`;
+}
